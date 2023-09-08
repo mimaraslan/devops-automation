4f8a5f59805b0116c5db3fad9cd9fca2457fac66
Mimar Aslan <mimaraslan@gmail.com>
2023-09-08 03:08:30 +0300

    DevOps Automation

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,3 @@
 
-Y
-)src/main/java/com/mimaraslan/AppMain.java,b\2\b2ea204ec389b62d2a98e3f7796a5d14eced6073
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- src/main/java/com/mimaraslan/AppMain.java
+++ src/main/java/com/mimaraslan/AppMain.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class AppMain {
 
 	@GetMapping
